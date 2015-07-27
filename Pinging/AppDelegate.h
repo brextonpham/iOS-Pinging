3a07927f0773c7a6f8bfa95daf2c1cfe9e2a90b8
Brexton Pham <brextonpham@stanford.edu>
2015-07-27 17:42:36 -0400

    Initial Commit

---
+++ Pinging/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pinging
+//
+//  Created by Brexton Pham on 7/27/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
